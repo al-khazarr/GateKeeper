ab4015315314d6f605be1c0fd02bc83cc1b096d6
al-khazarr <119543286+al-khazarr@users.noreply.github.com>
2023-03-25 15:44:48 +0300

    Delete cmd/app directory

--- cmd/app/_vendor.bat
+++
@@ -1,5 +0,0 @@
-go mod tidy
-rem go clean -modcache
-go get -u ./../...
-go mod vendor
-rem make -f make vendor
--- cmd/app/bench/goprofile.bat
+++
@@ -1,6 +0,0 @@
-go tool pprof goprofex http://127.0.0.1:3001/debug/pprof/profile
-rem go tool pprof goprofex http://127.0.0.1:3000/debug/pprof/goroutine
-rem go tool pprof -alloc_objects goprofex http://127.0.0.1:3000/debug/pprof/heap
-
-rem web
-rem top100 -cum (sync|runtime)
